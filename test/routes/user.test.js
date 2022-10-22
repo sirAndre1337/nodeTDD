610d354149d4f9cbbc0aa62b754233fe8648afa1
unknown <andrelluis17@hotmail.com>
2022-10-22 20:40:19 -0300

    Movendo o teste de users para a pasta routes

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest')
 
-const app = require('../src/app')
+const app = require('../../src/app')
 
 test('Deve listar todos os usuarios', () => {
   return request(app).get('/users')
