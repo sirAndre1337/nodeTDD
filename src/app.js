743d9b6ca8613bb0e47113f6ac9acfdb4ffbf98c
unknown <andrelluis17@hotmail.com>
2022-10-22 20:05:50 -0300

    Adicioando o knex-logger para verificar as consultas do BD

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const app = require('express')()
 const consign = require('consign')
 const knex = require('knex')
 const knexfile = require('../knexfile')
+// const knexLogger = require('knex-logger')
 
 // TODO criar chaveamento dinamico
 app.db = knex(knexfile.test)
+
+// app.use(knexLogger(app.db))   // mudar o verbose para false para ver os logs
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
