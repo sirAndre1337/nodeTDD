1b7c71a8bb805f3dea2f2f6c9e1faabe616ee2df
unknown <andrelluis17@hotmail.com>
2022-10-24 14:50:09 -0300

    End point Accounts criado, test para inserir nova conta

---
+++ test/routes/account.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest')
+const app = require('../../src/app')
+
+const MAIN_ROUTE = '/accounts';
+let user;
+
+beforeAll(async () => {
+  const result = await app.services.user.save({ name: 'User account', mail: `${Date.now()}@mail.com`, password: '123account' })
+  user = { ...result[0] }
+})
+
+test('Deve inserir uma conta com sucesso', () => {
+  const name = Date.now();
+
+  return request(app).post(MAIN_ROUTE)
+
+    .send({ name: `Acc ${name}`, user_id: user.id })
+    .then(res => {
+      expect(res.status).toBe(201)
+      expect(res.body.name).toBe(`Acc ${name}`)
+    })
+})
